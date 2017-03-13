431006062507dffb6a292ea73d488d240122f8f6
yclaide940 <786963531@qq.com>
2017-03-13 15:53:36 +0800

    Initial commit

---
+++ app/src/main/java/cn/ycl/com/viewpagerdiy/MainActivity.java
@@ -0,0 +1,108 @@
+package cn.ycl.com.viewpagerdiy;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import cn.ycl.com.viewpagerdiy.viewpager.MyViewPager;
+
+public class MainActivity extends AppCompatActivity {
+    private MyPageAdapter adapter;
+    private MyViewPager viewPager;
+    private int[] arr = {R.drawable.leimu1,R.drawable.leimu2,R.drawable.leim3};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (MyViewPager) findViewById(R.id.mvp);
+        adapter = new MyPageAdapter(arr);
+        viewPager.setAdapter(adapter);
+       //  viewPager.setPageTransformer(true,new ZoomOutPageTransformer());
+    }
+
+    private class MyPageAdapter extends PagerAdapter{
+        private int[] arr;
+        public MyPageAdapter(int[] arr){
+            this.arr =arr;
+        }
+        @Override
+        public int getCount() {
+            return arr.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==object;
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = new ImageView(MainActivity.this);
+            imageView.setImageResource(arr[position]);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            container.addView(imageView);
+            viewPager.addChlidView(position,imageView);
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+    }
+
+    public class ZoomOutPageTransformer implements ViewPager.PageTransformer
+    {
+        private static final float MIN_SCALE = 0.85f;
+        private static final float MIN_ALPHA = 0.5f;
+
+        @SuppressLint("NewApi")
+        public void transformPage(View view, float position)
+        {
+            int pageWidth = view.getWidth();
+            int pageHeight = view.getHeight();
+
+            Log.e("TAG", view + " , " + position + "");
+
+            if (position < -1)
+            { // [-Infinity,-1)
+                // This page is way off-screen to the left.
+                view.setAlpha(0);
+
+            } else if (position <= 1) //a页滑动至b页 ； a页从 0.0 -1 ；b页从1 ~ 0.0
+            { // [-1,1]
+                // Modify the default slide transition to shrink the page as well
+                float scaleFactor = Math.max(MIN_SCALE, 1 - Math.abs(position));
+                float vertMargin = pageHeight * (1 - scaleFactor) / 2;
+                float horzMargin = pageWidth * (1 - scaleFactor) / 2;
+                if (position < 0)
+                {
+                    view.setTranslationX(horzMargin - vertMargin / 2);
+                } else
+                {
+                    view.setTranslationX(-horzMargin + vertMargin / 2);
+                }
+
+                // Scale the page down (between MIN_SCALE and 1)
+                view.setScaleX(scaleFactor);
+                view.setScaleY(scaleFactor);
+
+                // Fade the page relative to its size.
+                view.setAlpha(MIN_ALPHA + (scaleFactor - MIN_SCALE)
+                        / (1 - MIN_SCALE) * (1 - MIN_ALPHA));
+
+            } else
+            { // (1,+Infinity]
+                // This page is way off-screen to the right.
+                view.setAlpha(0);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/cn/ycl/com/viewpagerdiy/viewpager/MyViewPager.java
@@ -0,0 +1,79 @@
+package cn.ycl.com.viewpagerdiy.viewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.nineoldandroids.view.ViewHelper;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yechenglong on 2017/3/13.
+ */
+
+public class MyViewPager extends ViewPager{
+    private View mleft;
+    private View mRight;
+    private float mTrans;
+    private static final float SCALE_MAX = 0.5f;
+    private Map<Integer,View> viewMap = new HashMap<>();
+    private float mScale;
+
+    public MyViewPager(Context context) {
+        super(context);
+    }
+
+    public MyViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onPageScrolled(int position, float offset, int offsetPixels) {
+        float effectoffset =  isSmall(offset)?0:offset;
+        mleft =getChlidView(position);
+        mRight = getChlidView(position+1);
+        animateStack(mleft,mRight,effectoffset,offsetPixels);
+        super.onPageScrolled(position, offset, offsetPixels);
+    }
+
+    private boolean isSmall(float positionOffset)
+    {
+        return Math.abs(positionOffset) < 0.0001;
+    }
+
+    private View getChlidView(int position){
+        return viewMap.get(position);
+    }
+
+    protected void animateStack(View left, View right, float effectOffset,
+                                int positionOffsetPixels)
+    {
+        if (right != null)
+        {
+            /**
+             * 缩小比例 如果手指从右到左的滑动（切换到后一个）：0.0~1.0，即从一半到最大
+             * 如果手指从左到右的滑动（切换到前一个）：1.0~0，即从最大到一半
+             */
+            mScale = (1 - SCALE_MAX) * effectOffset + SCALE_MAX;
+            /**
+             * x偏移量： 如果手指从右到左的滑动（切换到后一个）：0-720 如果手指从左到右的滑动（切换到前一个）：720-0
+             */
+            mTrans = -getWidth() - getPageMargin() + positionOffsetPixels;
+            ViewHelper.setScaleX(right, mScale);
+            ViewHelper.setScaleY(right, mScale);
+            ViewHelper.setTranslationX(right, mTrans);
+            ViewHelper.setAlpha(right,mScale);
+        }
+        if (left != null)
+        {
+            left.bringToFront();
+        }
+    }
+
+    public void addChlidView(int position,View view){
+        viewMap.put(position,view);
+    }
+}
